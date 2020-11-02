2c25432e3255c93c1cc64df6aee8f3587447e07d
JuanRiascos <jua.restrepo@udla.edu.co>
2020-11-01 20:10:29 -0500

    refactor(button):Add button properties

--- src/assets/components/Button/Button.js
+++ src/assets/components/Button/Button.js
@@ -1,15 +1,31 @@
 import React from 'react'
 
 const Button = ({ name, topic, ...props }) => {
-	return (
-		<button
-			{...props}
-			className={`${
-				topic ? 'button--yellow text-white' : 'text-gray-500 border-2'
-			} focus:outline-none font-bold hover:shadow-lg mb-2 outline-none p-2 py-3 rounded rounded-lg text-sm w-full`}>
-			{name}
-		</button>
-	)
+
+	const _handleSelectButton = () => {
+    console.log(topic,' dasd');
+		if (topic === 'main') {
+			return (
+				<button
+					className='focus:outline-none font-bold hover:shadow-lg bg-blue-500 text-white active:bg-blue-600  uppercase text-sm px-6 py-3 rounded shadow  outline-none  mr-1 mb-1'
+					type='button'
+					{...props}>
+					{name}
+				</button>
+			)
+		}
+
+		return (
+			<button
+				{...props}
+				className={`${
+					topic ? 'button--yellow text-white' : 'text-gray-500 border-2'
+				} focus:outline-none font-bold hover:shadow-lg mb-2 outline-none p-2 py-3 rounded rounded-lg text-sm w-full`}>
+				{name}
+			</button>
+		)
+	}
+	return <>{_handleSelectButton()}</>
 }
 
 export default Button
--- src/assets/views/app/App.js
+++ src/assets/views/app/App.js
@@ -1,17 +1,12 @@
 import React, { useState } from 'react'
+import Button from '../../components/Button/Button'
 import Modal from '../../components/Modal/Modal'
 function App() {
 	const [showModal, setShowModal] = useState(false)
 	return (
 		<div className='flex h-full items-center justify-center w-full'>
 			<div className='fixed button__helped'>
-				<button
-					className='bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1'
-					type='button'
-					style={{ transition: 'all .15s ease' }}
-					onClick={() => setShowModal(true)}>
-					Cancelar servicio
-				</button>
+				<Button name='Cancelar el servicio' topic='main' type='button' onClick={() => setShowModal(true)} />
 			</div>
 
 			{showModal && (
